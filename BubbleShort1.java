760de862fa797446c15061420ab797400b05d94b
Rutikchandore <138876742+Rutikchandore@users.noreply.github.com>
2023-08-29 22:58:21 +0530

    Update BubbleShort1.java

--- BubbleShort1.java
+++ BubbleShort1.java
@@ -1,63 +1,49 @@
 import java.util.*;
 
-class Displays
-{
-	public void dis(int arr[],int iValue)
-	{
-		for(int iCnt = 0; iCnt < iValue; iCnt++)
-		{
-			System.out.println(arr[iCnt]);
-		}
-	}
+class Displays {
+    public void displayArray(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
 
-class Sorting1 
-{
-	public void sorting(int arr[], int iValue)
-	{
-		for(int i = 0; i < iValue; i++)
-		{
-			for(int j = i + 1; j < iValue; j++)
-			{
-				if(arr[i] > arr[j])
-				{
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		
-	}
+class Sorting1 {
+    public void bubbleSort(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 }
 
-public class BubbleShort1 {
-	public static void main(String [] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter the Size of Array : ");
-		int iValue  = sc.nextInt();
-		
-		int[] arr = new int [iValue];
-		
-		System.out.println("Enter the array Element : ");
-		for(int iCnt = 0; iCnt < iValue; iCnt++)
-		{
-			arr[iCnt] =sc.nextInt();
-		}
-		
-		System.out.println("Before Array Sorting : ");
-		Displays ds = new Displays();
-		ds.dis(arr, iValue);
-		
-		Sorting1 s = new Sorting1();
-		s.sorting(arr, iValue);
-		
-		System.out.println("After Array Sorting :");
-		ds.dis(arr, iValue);
-		
-		
-	}
+public class BubbleSort1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter the Size of Array: ");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter the Array Elements: ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Before Array Sorting: ");
+        Displays displayObj = new Displays();
+        displayObj.displayArray(arr, size);
+
+        Sorting1 sortingObj = new Sorting1();
+        sortingObj.bubbleSort(arr, size);
+
+        System.out.println("After Array Sorting:");
+        displayObj.displayArray(arr, size);
+    }
 }
